f7aad23487b5fdfbdd7c2e604a5e3c05f17db0c9
Eddie Kao <eddie@digik.com.tw>
2015-04-19 11:36:17 +0800

    add product for front-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  resources :products
+
   namespace :backend do
     get 'dashboard', to:'dashboard#index'
     resources :products, except:[:show]
