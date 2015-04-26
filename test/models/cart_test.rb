700e2ac05a683153d7d11babf11be56907e750a9
Eddie Kao <eddie@digik.com.tw>
2015-04-26 10:10:15 +0800

    add test for cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
+  test "cart should be able to add something" do
+    cart = Cart.new
+    cart.add_item(1)
+
+    assert_equal false, cart.empty?
+  end
 end
